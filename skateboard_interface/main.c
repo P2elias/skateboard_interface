29e4ff84a8eed5e2b3525927d2aa5dfef9745e91
eliasR <elias.rotzler@students.fhnw.ch>
2018-06-15 14:35:41 +0200

    Initial commit

---
+++ skateboard_interface/main.c
@@ -0,0 +1,18 @@
+/*
+ * skateboard_interface.c
+ *
+ * Created: 15.06.2018 14:11:05
+ * Author : elias.rotzler
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
