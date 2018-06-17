f3fe17793cf35e415c05d1f2f7054d6c08844413
eliasR <elias.rotzler@students.fhnw.ch>
2018-06-17 15:22:24 +0200

    erste Anpassungen auf neue Funktionalitäten, auskommentierung des alten Codes

--- skateboard_interface/gpio.c
+++ skateboard_interface/gpio.c
@@ -4,37 +4,82 @@
 #define TRISTATE_INPUT 0
 #define TRISTATE_OUTPUT 1
 
-#define PORT_LEDS PORTA
-#define TRISTATE_LEDS DDRA
-#define PIN_LED0 1
-#define PIN_LED1 2
-#define PIN_LED2 3
-#define PIN_LED3 4
-
 #define PORT_POWER_LED PORTA
 #define TRISTATE_POWER_LED DDRA
 #define PIN_POWER_LED 0
+
+#define PORT_LEDS PORTA
+#define TRISTATE_LEDS DDRA
+#define PIN_LED2 1
+#define PIN_LED3 2
+#define PIN_LED4 3
+#define PIN_LED5 4
 
 #define PORT_PIEZO PORTA
 #define TRISTATE_PIEZO DDRA
 #define PIN_PIEZO 5
 
-#define PORT_BRIDGE_DRIVER PORTJ
-#define TRISTATE_BRIDGE_DRIVER DDRJ
-#define PIN_EN_GATE 0
-#define PIN_PWRGD 4
-#define PIN_nOCTW 3
-#define PIN_nFAULT 2
-#define PIN_DC_CAL 1
+#define PORT_EN_CNTRLLR PORTG
+#define TRISTATE_EN_CNTRLLR DDRG
+#define PIN_EN_CNTRLLR 4
+
+#define PORT_VBAT_PVDD2_SCHALTER PORTL
+#define TRISTATE_VBAT_PVDD2_SCHALTER DDRL
+#define PIN_VBAT_PVDD2_SCHALTER 1
+
+#define PORT_TASTER PORTL
+#define TRISTATE_TASTER DDRL
+#define PIN_TASTER 2
+
+#define PORT_SELBSTHALT_SCHALTER PORTL
+#define TRISTATE_SELBSTHALT_SCHALTER DDRL
+#define PIN_SELBSTHALT_SCHALTER 3
+
+#define PORT_SOLL_MOMENT PORTK
+#define TRISTATE_SOLL_MOMENT DDRK
+#define PIN_SOLL_MOMENT 0
+
+#define PORT_HALL_NOSE PORTK
+#define TRISTATE_HALL_NOSE DDRK
+#define PIN_HALL_NOSE 1
+
+#define PORT_HALL_TAIL PORTK
+#define TRISTATE_HALL_TAIL DDRK
+#define PIN_HALL_TAIL 2
+
+#define PORT_PVDD_SENSE PORTK
+#define TRISTATE_PVDD_SENSE DDRK
+#define PIN_PVDD_SENSE 4
+
+#define PORT_ABGLEICH PORTF
+#define TRISTATE_ABGLEICH DDRA
+#define PIN_ABGLEICH_FAHRER_GEWICHT 1
+#define PIN_ABGLEICH_BAT_MIN 2
+#define PIN_ABGLEICH_BAT_MAX 3
+#define PIN_ABGLEICH_TAIL_NEUTRAL 4
+#define PIN_ABGLEICH_TAIL_FULL 5
+#define PIN_ABGLEICH_NOSE_NEUTRAL 6
+#define PIN_ABGLEICH_NOSE_FULL 7
+
+//#define PORT_BRIDGE_DRIVER PORTJ
+//#define TRISTATE_BRIDGE_DRIVER DDRJ
+//#define PIN_EN_GATE 0
+//#define PIN_PWRGD 4
+//#define PIN_nOCTW 3
+//#define PIN_nFAULT 2
+//#define PIN_DC_CAL 1
 
 void initGPIOs()
 {
     //logMsgLn("Init GPIOs...");
 
-    TRISTATE_LEDS |= ((TRISTATE_OUTPUT<<PIN_LED0) | (TRISTATE_OUTPUT<<PIN_LED1) | (TRISTATE_OUTPUT<<PIN_LED2) | (TRISTATE_OUTPUT<<PIN_LED3));
+    TRISTATE_LEDS |= ((TRISTATE_OUTPUT<<PIN_LED2) | (TRISTATE_OUTPUT<<PIN_LED3) | (TRISTATE_OUTPUT<<PIN_LED4) | (TRISTATE_OUTPUT<<PIN_LED5));
     TRISTATE_POWER_LED |= (TRISTATE_OUTPUT<<PIN_POWER_LED);
     TRISTATE_PIEZO |= (TRISTATE_OUTPUT<<PIN_PIEZO);
-    TRISTATE_BRIDGE_DRIVER |= ((TRISTATE_OUTPUT<<PIN_EN_GATE) | (TRISTATE_INPUT<<PIN_PWRGD) | (TRISTATE_INPUT<<PIN_nFAULT) | (TRISTATE_INPUT<<PIN_DC_CAL));
+    TRISTATE_EN_CNTRLLR |= (TRISTATE_OUTPUT<<PIN_EN_CNTRLLR);
+	TRISTATE_VBAT_PVDD2_SCHALTER |= ((TRISTATE_OUTPUT<<PIN_VBAT_PVDD2_SCHALTER) | (TRISTATE_OUTPUT<<PIN_TASTER) | (TRISTATE_OUTPUT<<PIN_SELBSTHALT_SCHALTER));
+	TRISTATE_SOLL_MOMENT |= ((TRISTATE_INPUT<<PIN_SOLL_MOMENT) | (TRISTATE_INPUT<<PIN_HALL_NOSE) | (TRISTATE_INPUT<<PIN_HALL_TAIL) | (TRISTATE_INPUT<<PIN_PVDD_SENSE));
+	TRISTATE_ABGLEICH |= ((TRISTATE_INPUT<<PIN_ABGLEICH_FAHRER_GEWICHT) | (TRISTATE_INPUT<<PIN_ABGLEICH_BAT_MIN) | (TRISTATE_ABGLEICH<<PIN_ABGLEICH_BAT_MAX) | (TRISTATE_ABGLEICH<<PIN_ABGLEICH_TAIL_NEUTRAL) | (TRISTATE_ABGLEICH<<PIN_ABGLEICH_TAIL_FULL) | (TRISTATE_ABGLEICH<<PIN_ABGLEICH_NOSE_NEUTRAL) | (TRISTATE_ABGLEICH<<PIN_ABGLEICH_NOSE_FULL));
 }
 
 /** sets the 4 leds to visualize the battery power
@@ -48,6 +93,24 @@ void setLEDsBatteryPower(char batteryPower)
 {
     if(batteryPower>3)
     {
+        PORT_LEDS |= (1<<PIN_LED5);
+    }
+    else
+    {
+        PORT_LEDS &= ~(1<<PIN_LED5);	// müsste hier (und auch sonst überall) nicht mit Maske gearbeitet 
+    }									// werden, da sonst ALLE ports (pins) des PORT_LEDS auf 0 gesetzt werden?
+
+    if(batteryPower>2)
+    {
+        PORT_LEDS |= (1<<PIN_LED4);
+    }
+    else
+    {
+        PORT_LEDS &= ~(1<<PIN_LED4);
+    }
+
+    if(batteryPower>1)
+    {
         PORT_LEDS |= (1<<PIN_LED3);
     }
     else
@@ -55,7 +118,7 @@ void setLEDsBatteryPower(char batteryPower)
         PORT_LEDS &= ~(1<<PIN_LED3);
     }
 
-    if(batteryPower>2)
+    if(batteryPower>0)
     {
         PORT_LEDS |= (1<<PIN_LED2);
     }
@@ -63,43 +126,57 @@ void setLEDsBatteryPower(char batteryPower)
     {
         PORT_LEDS &= ~(1<<PIN_LED2);
     }
-
-    if(batteryPower>1)
-    {
-        PORT_LEDS |= (1<<PIN_LED1);
-    }
-    else
-    {
-        PORT_LEDS &= ~(1<<PIN_LED1);
-    }
-
-    if(batteryPower>0)
-    {
-        PORT_LEDS |= (1<<PIN_LED0);
-    }
-    else
-    {
-        PORT_LEDS &= ~(1<<PIN_LED0);
-    }
 }
 
-/** enable bridge driver
+/** enable Selbsthaltung
+state >= 1: turn SELBSTHALTUNG_SCHALTER on
+state = 0: turn SELBSTHALTUNG_SCHALTER off
+**/
+void enableSelbsthaltung(char state)
+{
+	if(state)
+	{
+		PORT_SELBSTHALT_SCHALTER |= (1<<PIN_SELBSTHALT_SCHALTER);
+	}
+	else
+	{
+		PORT_SELBSTHALT_SCHALTER &= ~(1<<PIN_SELBSTHALT_SCHALTER);
+	}
+}
+
+/** enable Motor-controller (CNTRLLR)
 state >= 1: turn PWM on
 state = 0: turn PWM off
 **/
-void enableBridgeDriver(char state)
+void enableMotorController(char state)
 {
     if(state)
     {
-        PORT_BRIDGE_DRIVER |= (1<<PIN_EN_GATE);
+        PORT_EN_CNTRLLR |= (1<<PIN_EN_CNTRLLR);
     }
     else
     {
-        PORT_BRIDGE_DRIVER &= ~(1<<PIN_EN_GATE);
+        PORT_EN_CNTRLLR &= ~(1<<PIN_EN_CNTRLLR);
     }
 }
 
-void setDC_cal(uint8_t state)
+/** enable Power to Motor-controller
+state >= 1: turn VBAT+_PVDD2_SCHALTER on
+state = 0: turn VBAT+_PVDD2_SCHALTER off
+**/
+void enablePVDD2(char state)
+{
+	if(state)
+	{
+		PORT_VBAT_PVDD2_SCHALTER |= (1<<PIN_VBAT_PVDD2_SCHALTER);
+	}
+	else
+	{
+		PORT_VBAT_PVDD2_SCHALTER &= ~(1<<PIN_VBAT_PVDD2_SCHALTER);
+	}
+}
+
+/*void setDC_cal(uint8_t state)
 {
     if(state)
     {
@@ -109,7 +186,7 @@ void setDC_cal(uint8_t state)
     {
         PORT_BRIDGE_DRIVER &= ~(1<<PIN_DC_CAL);
     }
-}
+}*/
 
 /** Turns on Power LED
 **/
